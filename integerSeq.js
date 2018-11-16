da698db040956ce5a94d7f8db9bfb68c83cbc181
loangelak <loangela@optonline.net>
2018-11-16 08:50:40 -0500

    Create integerSeq.js

---
+++ integerSeq.js
@@ -0,0 +1,7 @@
+//Create a program that, when run, would display all integers from 1 to ∞ (or any relevant implementation limit),   
+//in sequence   (i.e.   1, 2, 3, 4, etc)   if given enough time.
+
+var i = 0;
+while(true){
+  console.log(i++ + ' ');
+}
